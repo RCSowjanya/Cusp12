770a028f0532f338cc174212bf444a6761f9c5bc
RCSowjanya <145266658+RCSowjanya@users.noreply.github.com>
2024-09-04 07:53:48 +0530

    Update PeakLoad.jsx

--- src/Components/PeakLoad.jsx
+++ src/Components/PeakLoad.jsx
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { FaRegUser } from "react-icons/fa";
 
-const PeakLoad = ({ formData, handlePeakLoadChange = () => {} }) => {
+const PeakLoad = ({ formData, errors, handlePeakLoadChange = () => {} }) => {
   // State to manage multiple field sets
   const [fieldSets, setFieldSets] = useState(
-    formData?.load?.length > 0 ? formData.load : [{ id: Date.now(), equipmentName: "", capacity: "", operation: "", equipments: "" }]
+    formData?.load?.length > 0
+      ? formData.load
+      : [
+          {
+            id: Date.now(),
+            equipmentName: "",
+            capacity: "",
+            operation: "",
+            equipments: "",
+          },
+        ]
   );
 
   // Whenever formData.load changes, update the fieldSets state
@@ -16,12 +26,12 @@ const PeakLoad = ({ formData, handlePeakLoadChange = () => {} }) => {
 
   // Function to handle adding a new set of fields
   const addFieldSet = () => {
-    const newFieldSet = { 
+    const newFieldSet = {
       id: Date.now(),
       equipmentName: "",
       capacity: "",
       operation: "",
-      equipments: ""
+      equipments: "",
     };
     const updatedFieldSets = [...fieldSets, newFieldSet];
     setFieldSets(updatedFieldSets);
@@ -41,10 +51,12 @@ const PeakLoad = ({ formData, handlePeakLoadChange = () => {} }) => {
 
   return (
     <div className="relative">
-      <h2 className="text-[#004A9C] font-[600] text-[16px] text-center pb-6 pt-4">
+      <h2 className="text-[#004A9C] font-[600] text-[16px] text-center p-3">
         Peak Load
       </h2>
-
+      <p className="text-center pb-3 text-[14px]">
+        Please provide a complete list of appliances and their usage
+      </p>
       {/* Scrollable container */}
       <div
         className="scrollable-container relative overflow-y-auto"
@@ -56,61 +68,88 @@ const PeakLoad = ({ formData, handlePeakLoadChange = () => {} }) => {
             className="flex flex-col gap-5 w-full max-[1000px]:flex-col mb-5 border border-gray-400 rounded-md p-4"
           >
             <div className="flex max-[1100px]:flex-col gap-5 w-full max-[1000px]:flex-col">
-              <div className="flex items-center border w-full border-[#CDC4B1] rounded-md bg-[#FFFDF9] quote">
-                <div className="border-r border-r-[#8A6112] p-2">
-                  <FaRegUser className="text-[#8A6112] ml-3" />
+              <div className="w-full">
+                <div className="flex items-center border w-full border-[#CDC4B1] rounded-md bg-[#FFFDF9] quote">
+                  <div className="border-r border-r-[#8A6112] p-2">
+                    <FaRegUser className="text-[#8A6112] ml-3" />
+                  </div>
+                  <input
+                    type="text"
+                    name="equipmentName"
+                    value={fieldSet.equipmentName}
+                    onChange={(e) => handleChange(e, fieldSet.id)}
+                    placeholder="Appliance Name"
+                    className="w-full px-4 py-5 font-[400] rounded-md text-[16px] shadow-lg placeholder-[#8A6112] outline-none"
+                  />
                 </div>
-                <input
-                  type="text"
-                  name="equipmentName"
-                  value={fieldSet.equipmentName}
-                  onChange={(e) => handleChange(e, fieldSet.id)}
-                  placeholder="Equipment Name"
-                  className="w-full px-4 py-5 font-[400] rounded-md text-[16px] shadow-lg placeholder-[#8A6112] outline-none"
-                />
+                {errors.equipmentName && (
+                  <p className="text-red-500 text-sm mt-1 pl-4">
+                    {errors.equipmentName}
+                  </p>
+                )}
               </div>
-
-              <div className="flex items-center border w-full border-[#CDC4B1] rounded-md bg-[#FFFDF9] quote">
-                <div className="border-r border-r-[#8A6112] p-2">
-                  <FaRegUser className="text-[#8A6112] ml-3" />
+              <div className="w-full">
+                <div className="flex items-center border w-full border-[#CDC4B1] rounded-md bg-[#FFFDF9] quote">
+                  <div className="border-r border-r-[#8A6112] p-2">
+                    <FaRegUser className="text-[#8A6112] ml-3" />
+                  </div>
+                  <input
+                    type="number"
+                    name="capacity"
+                    value={fieldSet.capacity}
+                    onChange={(e) => handleChange(e, fieldSet.id)}
+                    placeholder="Watt Capacity"
+                    className="w-full px-4 py-5 text-[16px] font-[400] rounded-md shadow-sm placeholder-[#8A6112] outline-none focus:outline-none"
+                  />
                 </div>
-                <input
-                  type="number"
-                  name="capacity"
-                  value={fieldSet.capacity}
-                  onChange={(e) => handleChange(e, fieldSet.id)}
-                  placeholder="Watt Capacity"
-                  className="w-full px-4 py-5 text-[16px] font-[400] rounded-md shadow-sm placeholder-[#8A6112] outline-none focus:outline-none"
-                />
+                {errors.capacity && (
+                  <p className="text-red-500 text-sm mt-1 pl-4">
+                    {errors.capacity}
+                  </p>
+                )}
               </div>
             </div>
-            <div className="flex max-[1100px]:flex-col gap-5 w-full max-[1000px]:flex-col">
-              <div className="flex items-center border w-full border-[#CDC4B1] rounded-md bg-[#FFFDF9] quote">
-                <div className="border-r border-r-[#8A6112] p-2">
-                  <FaRegUser className="text-[#8A6112] ml-3" />
-                </div>
-                <input
-                  type="number"
-                  name="operation"
-                  value={fieldSet.operation}
-                  onChange={(e) => handleChange(e, fieldSet.id)}
-                  placeholder="Hours of Operation/Day"
-                  className="w-full px-4 py-5 text-[16px] font-[400] rounded-md shadow-sm placeholder-[#8A6112] outline-none focus:outline-none"
-                />
-              </div>
 
-              <div className="flex items-center border w-full border-[#CDC4B1] rounded-md bg-[#FFFDF9] quote">
-                <div className="border-r border-r-[#8A6112] p-2">
-                  <FaRegUser className="text-[#8A6112] ml-3" />
+            <div className="flex max-[1100px]:flex-col gap-5 w-full max-[1000px]:flex-col">
+              <div className="w-full">
+                <div className="flex items-center border w-full border-[#CDC4B1] rounded-md bg-[#FFFDF9] quote">
+                  <div className="border-r border-r-[#8A6112] p-2">
+                    <FaRegUser className="text-[#8A6112] ml-3" />
+                  </div>
+                  <input
+                    type="number"
+                    name="operation"
+                    value={fieldSet.operation}
+                    onChange={(e) => handleChange(e, fieldSet.id)}
+                    placeholder="Hours of Operation/Day"
+                    className="w-full px-4 py-5 text-[16px] font-[400] rounded-md shadow-sm placeholder-[#8A6112] outline-none focus:outline-none"
+                  />
                 </div>
-                <input
-                  type="number"
-                  name="equipments"
-                  value={fieldSet.equipments}
-                  onChange={(e) => handleChange(e, fieldSet.id)}
-                  placeholder="No. of Equipment"
-                  className="w-full px-4 py-5 text-[16px] font-[400] rounded-md shadow-sm placeholder-[#8A6112] outline-none focus:outline-none"
-                />
+                {errors.operation && (
+                  <p className="text-red-500 text-sm mt-1 pl-4">
+                    {errors.operation}
+                  </p>
+                )}
+              </div>
+              <div className="w-full">
+                <div className="flex items-center border w-full border-[#CDC4B1] rounded-md bg-[#FFFDF9] quote">
+                  <div className="border-r border-r-[#8A6112] p-2">
+                    <FaRegUser className="text-[#8A6112] ml-3" />
+                  </div>
+                  <input
+                    type="number"
+                    name="equipments"
+                    value={fieldSet.equipments}
+                    onChange={(e) => handleChange(e, fieldSet.id)}
+                    placeholder="No. of Equipment"
+                    className="w-full px-4 py-5 text-[16px] font-[400] rounded-md shadow-sm placeholder-[#8A6112] outline-none focus:outline-none"
+                  />
+                </div>
+                {errors.equipments && (
+                  <p className="text-red-500 text-sm mt-1 pl-4">
+                    {errors.equipments}
+                  </p>
+                )}
               </div>
             </div>
           </div>
@@ -120,7 +159,7 @@ const PeakLoad = ({ formData, handlePeakLoadChange = () => {} }) => {
       {/* Fixed Add Button */}
       <button
         onClick={addFieldSet}
-        className="absolute right-0 top-1/2 transform -translate-y-1/2 w-12 h-12 bg-[#004A9C] text-white rounded-full text-2xl flex items-center justify-center min-[700px]:right-0 max-[600px]:left-1/2 max-[600px]:transform max-[600px]:-translate-x-1/2 max-[600px]:bottom-0 max-[600px]:mx-auto"
+        className="absolute right-0 top-1/2 transform -translate-y-1/2 w-12 h-12 bg-[#0bb68d] text-white  rounded-full text-2xl flex items-center justify-center min-[700px]:right-0 max-[600px]:left-1/2 max-[600px]:transform max-[600px]:-translate-x-1/2 max-[600px]:bottom-0 max-[600px]:mx-auto"
       >
         +
       </button>
